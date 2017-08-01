ee9b4e891f51bb94a790c1a4959b261bc013629c
Mickael <polmickael3@gmail.comgit config --global user.name>
2017-08-01 13:32:15 +0200

    fin exo cookie clicker

--- app.js
+++ app.js
@@ -1,5 +1,24 @@
-$(document).ready(function(){
-	//votre code
 
+$('document').ready(function(){
+	var count = 0;
+	var alea = ["fait autre chose de ta vie ", 
+	"clique ailleurs pleaz", 
+	"stop la salsifi", 
+	"bon ça commence a me gonfler !! :@",
+	"aie aie stop me cliquer dessus c'est bon!"]
+	
+	$('.cake-is-a-lie').click(function() {
+		// console.log('sa clique');
+		count++;
+		// console.log(count);
+		$('.counter').text(count);
 
+		var random = Math.random()* alea.length;
+		random = Math.floor(random);
+		alea [random];
+		
+		if (count%5 == 0) {
+			alert(alea[random]);
+		}
+	})
 });
